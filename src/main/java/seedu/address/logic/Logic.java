c18cff908676150d6b9d41753b6b15867296adb3
Wong Pei Xian <peixianwong@gmail.com>
2021-09-27 00:42:56 +0800

    Update src/main/java/seedu/address/logic/Logic.java
    Co-authored-by: Zhang Guangxuan <zhangguangxuan99@gmail.com>

--- src/main/java/seedu/address/logic/Logic.java
+++ src/main/java/seedu/address/logic/Logic.java
@@ -31,7 +31,7 @@ public interface Logic {
     ReadOnlyAddressBook getAddressBook();
 
     /** Returns an unmodifiable view of the filtered list of properties */
-    ObservableList<Property> getFilteredPersonList();
+    ObservableList<Property> getFilteredPropertyList();
 
     /**
      * Returns the user prefs' address book file path.
