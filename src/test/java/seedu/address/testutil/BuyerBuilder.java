eccbb7f96d9396cf2bb8356d8b690302ee93611c
trxe <peixianwong@gmail.com>
2021-10-04 08:57:27 +0800

    Add BuyerBuilder and TypicalBuyers

---
+++ src/test/java/seedu/address/testutil/BuyerBuilder.java
@@ -0,0 +1,49 @@
+package seedu.address.testutil;
+
+import seedu.address.model.field.Email;
+import seedu.address.model.field.Name;
+import seedu.address.model.field.Person;
+import seedu.address.model.field.Phone;
+import seedu.address.model.field.Price;
+import seedu.address.model.property.Buyer;
+
+public class BuyerBuilder {
+
+    public static final Person DEFAULT_PERSON = TypicalPersons.ALICE;
+    public static final String DEFAULT_PRICE = "1230000";
+
+    private Person person;
+    private Price max;
+    /**
+     * Creates a {@code PersonBuilder} with the default details.
+     */
+    public BuyerBuilder() {
+        person = DEFAULT_PERSON;
+        max = new Price(DEFAULT_PRICE);
+    }
+
+    /**
+     * Initializes the BuyerBuilder with the data of {@code buyerToCopy}.
+     */
+    public BuyerBuilder(Buyer buyerToCopy) {
+        person = buyerToCopy.getPerson();
+    }
+
+    /**
+     * Sets the maximum {@code Price} of the {@code Buyer} that we are building.
+     */
+    public BuyerBuilder withPrice(String max) {
+        this.max = new Price(max);
+        return this;
+    }
+
+    public BuyerBuilder withPerson(Person person) {
+        this.person = person;
+        return this;
+    }
+
+    public Buyer build() {
+        return new Buyer(person, max);
+    }
+
+}
---
+++ src/test/java/seedu/address/testutil/TypicalBuyers.java
@@ -0,0 +1,47 @@
+package seedu.address.testutil;
+
+import seedu.address.model.field.Person;
+import seedu.address.model.property.Buyer;
+
+import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
+
+/**
+ * A utility class containing a list of {@code Buyer} objects to be used in tests.
+ */
+public class TypicalBuyers {
+
+    // Uses TypicalPersons to build TypicalBuyers
+    public static final Buyer ALICE = new BuyerBuilder().withPerson(seedu.address.testutil.TypicalPersons.ALICE)
+            .withPrice("123000").build();
+    public static final Buyer BENSON = new BuyerBuilder().withPerson(seedu.address.testutil.TypicalPersons.BENSON)
+            .withPrice("1231000").build();
+    public static final Buyer CARL = new BuyerBuilder().withPerson(seedu.address.testutil.TypicalPersons.CARL)
+            .withPrice("678000").build();
+    public static final Buyer DANIEL = new BuyerBuilder().withPerson(seedu.address.testutil.TypicalPersons.DANIEL)
+            .withPrice("102200").build();
+    public static final Buyer ELLE = new BuyerBuilder().withPerson(seedu.address.testutil.TypicalPersons.ELLE)
+            .withPrice("999000").build();
+    public static final Buyer FIONA = new BuyerBuilder().withPerson(seedu.address.testutil.TypicalPersons.FIONA)
+            .withPrice("999999").build();
+
+    // Manually added
+
+    // Manually added - Buyer's details found in {@code CommandTestUtil}
+    public static final Buyer AMY = new BuyerBuilder().withPerson(
+            new PersonBuilder().withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY).withEmail(VALID_EMAIL_AMY).build())
+            .withPrice("999999").build();
+    public static final Buyer BOB = new BuyerBuilder().withPerson(
+            new PersonBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB).build())
+            .withPrice("111111").build();
+
+    public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
+
+    private TypicalBuyers() {
+    } // prevents instantiation
+
+}
