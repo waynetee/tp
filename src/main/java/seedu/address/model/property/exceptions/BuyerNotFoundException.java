fac1bb432965d46be0efa529f9744a56c76d59b5
trxe <peixianwong@gmail.com>
2021-10-11 21:49:44 +0800

    Correct dcumentation

--- src/main/java/seedu/address/model/property/exceptions/BuyerNotFoundException.java
+++ src/main/java/seedu/address/model/property/exceptions/BuyerNotFoundException.java
@@ -1,6 +1,6 @@
 package seedu.address.model.property.exceptions;
 
 /**
- * Signals that the operation is unable to find the specified property.
+ * Signals that the operation is unable to find the specified buyer.
  */
 public class BuyerNotFoundException extends ListableNotFoundException {}
