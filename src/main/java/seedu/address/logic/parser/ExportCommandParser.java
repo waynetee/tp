9c05f9bb5fb07ede63b5eec8331ab9732f154ac3
elizabethchow1234@gmail.com <elizabethchow1234@gmail.com>
2021-10-18 09:04:40 +0800

    Fix ExportCommandParser

--- src/main/java/seedu/address/logic/parser/ExportCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ExportCommandParser.java
@@ -5,15 +5,15 @@ import static seedu.address.logic.parser.ParserUtil.MESSAGE_INVALID_ACTOR;
 import seedu.address.logic.commands.ExportCommand;
 import seedu.address.logic.commands.buyer.ExportBuyersCommand;
 import seedu.address.logic.commands.property.ExportPropertiesCommand;
-import seedu.address.logic.parser.PreambleData.Actor;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.field.Actor;
 
 /**
  * Parses input arguments and creates a new ExportCommand object
  */
 public class ExportCommandParser {
-    private static final int NUMBER_OF_PREAMBLE_ARGUMENTS = 1;
-
+    private static final int NUM_OF_PREAMBLE_ARGUMENTS = 1;
+    public static final int ACTOR_POSITIONAL_INDEX = 0;
     /**
      * Parses the given {@code String} of arguments in the context of the ExportCommand
      * and returns an ExportCommand object for execution.
@@ -22,9 +22,8 @@ public class ExportCommandParser {
      */
     public ExportCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args);
-
-        PreambleData preambleData = ParserUtil.parsePreamble(argMultimap.getPreamble(), NUMBER_OF_PREAMBLE_ARGUMENTS);
-        Actor actor = preambleData.getActor();
+        ParserUtil.assertPreambleArgsCount(argMultimap.getPreamble(), NUM_OF_PREAMBLE_ARGUMENTS);
+        Actor actor = ParserUtil.parseActor(args, ACTOR_POSITIONAL_INDEX);
         switch (actor) {
         case PROPERTY:
             return new ExportPropertiesCommand();
