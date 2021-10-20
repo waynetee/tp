2e63915f1f4d6daf4089fd9c645651a0071f33b2
Zhang Guangxuan <zhangguangxuan99@gmail.com>
2021-10-20 13:45:32 +0800

    Add Tests

--- src/main/java/seedu/address/storage/CsvManager.java
+++ src/main/java/seedu/address/storage/CsvManager.java
@@ -1,11 +1,6 @@
 package seedu.address.storage;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PRICE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.io.File;
 import java.io.FileReader;
@@ -40,21 +35,23 @@ import seedu.address.model.tag.Tag;
  * Manages exporting to or importing from csv.
  */
 public class CsvManager {
-    private static final String HEADER_NAME = "Name";
-    private static final String HEADER_PHONE = "Phone";
-    private static final String HEADER_EMAIL = "Email";
-    private static final String HEADER_ADDRESS = "Address";
-    private static final String HEADER_SELLER = "Seller Name";
-    private static final String HEADER_PRICE = "Price";
-    private static final String HEADER_BUDGET = "budget";
-    private static final String HEADER_TAGS = "Tags";
+    public static final String HEADER_NAME = "Name";
+    public static final String HEADER_PHONE = "Phone";
+    public static final String HEADER_EMAIL = "Email";
+    public static final String HEADER_ADDRESS = "Address";
+    public static final String HEADER_SELLER = "Seller Name";
+    public static final String HEADER_PRICE = "Price";
+    public static final String HEADER_BUDGET = "Budget";
+    public static final String HEADER_TAGS = "Tags";
 
     private static final String[] propertyHeaders = {HEADER_NAME, HEADER_ADDRESS, HEADER_SELLER,
             HEADER_PHONE, HEADER_EMAIL, HEADER_PRICE, HEADER_TAGS};
 
     private static final String[] buyerHeaders = {HEADER_NAME, HEADER_PHONE, HEADER_EMAIL, HEADER_BUDGET, HEADER_TAGS};
 
-    private static final String INVALID_CSV_FORMAT = "Invalid csv format!";
+    public static final String MESSAGE_INVALID_CSV_FORMAT = "Invalid csv format!";
+    public static final String MESSAGE_MISSING_HEADER = "Missing Header: ";
+    public static final String MESSAGE_NO_ENTRIES = "No recognized entries within csv!";
 
 
     /**
@@ -105,7 +102,7 @@ public class CsvManager {
     private static Property getProperty(Map<String, String> values) throws ParseException {
         for (String header : propertyHeaders) {
             if (!values.containsKey(header)) {
-                throw new ParseException("missing entry in importProperties: " + header);
+                throw new ParseException(MESSAGE_MISSING_HEADER + header);
             }
         }
         Name propertyName = ParserUtil.parseName(values.get(HEADER_NAME));
@@ -127,7 +124,7 @@ public class CsvManager {
     private static Buyer getBuyer(Map<String, String> values) throws ParseException {
         for (String header : buyerHeaders) {
             if (!values.containsKey(header)) {
-                throw new ParseException("missing entry in importBuyers: " + header);
+                throw new ParseException(MESSAGE_MISSING_HEADER + header);
             }
         }
         Name name = ParserUtil.parseName(values.get(HEADER_NAME));
@@ -152,7 +149,13 @@ public class CsvManager {
      */
     public static List<Property> importProperties(File file) throws IOException, ParseException {
         requireAllNonNull(file);
-        CSVReaderHeaderAware reader = new CSVReaderHeaderAware(new FileReader(file));
+        CSVReaderHeaderAware reader;
+        try {
+            reader = new CSVReaderHeaderAware(new FileReader(file));
+        }
+        catch (NullPointerException ex) {
+            throw new ParseException(MESSAGE_INVALID_CSV_FORMAT);
+        }
         Map<String, String> values;
         List<Property> properties = new ArrayList<>();
         try {
@@ -160,7 +163,12 @@ public class CsvManager {
                 properties.add(getProperty(values));
             }
         } catch (CsvValidationException e) {
-            throw new ParseException(INVALID_CSV_FORMAT);
+            throw new ParseException(MESSAGE_INVALID_CSV_FORMAT);
+        } catch (IOException ioe) {
+            throw new ParseException(MESSAGE_INVALID_CSV_FORMAT, ioe);
+        }
+        if (properties.isEmpty()) {
+            throw new ParseException(MESSAGE_NO_ENTRIES);
         }
         return properties;
     }
@@ -174,7 +182,13 @@ public class CsvManager {
      */
     public static List<Buyer> importBuyers(File file) throws IOException, ParseException {
         requireAllNonNull(file);
-        CSVReaderHeaderAware reader = new CSVReaderHeaderAware(new FileReader(file));
+        CSVReaderHeaderAware reader;
+        try {
+            reader = new CSVReaderHeaderAware(new FileReader(file));
+        }
+        catch (NullPointerException ex) {
+            throw new ParseException(MESSAGE_INVALID_CSV_FORMAT);
+        }
         Map<String, String> values;
         List<Buyer> buyers = new ArrayList<>();
         try {
@@ -182,7 +196,12 @@ public class CsvManager {
                 buyers.add(getBuyer(values));
             }
         } catch (CsvValidationException e) {
-            throw new ParseException(INVALID_CSV_FORMAT);
+            throw new ParseException(MESSAGE_INVALID_CSV_FORMAT);
+        } catch (IOException ioe) {
+            throw new ParseException(MESSAGE_INVALID_CSV_FORMAT, ioe);
+        }
+        if (buyers.isEmpty()) {
+            throw new ParseException(MESSAGE_NO_ENTRIES);
         }
         return buyers;
     }
