891d82d047727fd8e16011baa0a5b6686be2b1e4
Wayne Tee <waynetee37@gmail.com>
2021-10-20 14:28:32 +0800

    Tweak match command messages

--- src/main/java/seedu/address/logic/commands/MatchCommand.java
+++ src/main/java/seedu/address/logic/commands/MatchCommand.java
@@ -9,7 +9,7 @@ public abstract class MatchCommand extends SimpleCommand {
     public static final String COMMAND_WORD = "match";
 
     public static final String MESSAGE_USAGE =
-            COMMAND_WORD + ": Matches buyers and properties based on tags and price\n"
+            COMMAND_WORD + ": Matches buyers and properties based on tags and price.\n"
                     + "Parameters: ( auto | buyer INDEX | property INDEX )\n"
                     + "Example: " + COMMAND_WORD + " auto";
 
